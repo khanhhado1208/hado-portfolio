fc9d2de5d3bfdf50c686f373b7ce35717ef0938b
unknown <khanhhado1208@gmail.com>
2024-02-21 01:13:21 +0200

    add workflow

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,6 +5,7 @@ export default defineConfig({
   plugins: [
     react(),
   ],
+  base: "/hado-portfolio/",
   server: {
     port: 8000
   },
